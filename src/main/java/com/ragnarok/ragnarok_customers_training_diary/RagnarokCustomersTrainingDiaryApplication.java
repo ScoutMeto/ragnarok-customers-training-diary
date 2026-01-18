cddc9a23d9c79f3ce54adbfea687653656892b3e
Matej <m.pella@seznam.cz>
2026-01-18 17:37:44 +0100

    first motion

--- src/main/java/com/ragnarok/ragnarok_customers_training_diary/RagnarokCustomersTrainingDiaryApplication.java
+++ src/main/java/com/ragnarok/ragnarok_customers_training_diary/RagnarokCustomersTrainingDiaryApplication.java
@@ -1,8 +1,10 @@
-package com.ragnarok.ragnarokcustomerstrainingdiary;
+package com.ragnarok.ragnarok_customers_training_diary;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
 
+@EntityScan("com.matejmarek.ragnarok_customers_training_diary.entity")
 @SpringBootApplication
 public class RagnarokCustomersTrainingDiaryApplication {
 
@@ -11,3 +13,17 @@ public class RagnarokCustomersTrainingDiaryApplication {
     }
 
 }
+
+
+/*
+TODO - Řešíš trainingEntity a exerciseEntity(a jejich DTO)
+-promysli, co za data chceš sbírat
+-inspiruj se u minulého projektu, kde jsi už třídy vytvořil (chce to rozšířit a zpřesnit) -> složku máš otevřenou v PC (C:\Users\mpell\Desktop\IntelliJ IDEA projects\Ragnarok_TrainingDiaryAndReservationSystem - backend only\system_10-6-2023\ragnarokproject\src\main\java\com\mykettlebellproject\ragnarokproject\entity)
+-logika zacházení s daty (ukládání, změny, načítání, delete)
+-ověř propojení tabulek
+
+-app.properties, pom.xml, AdminInitializer, WebConfiguration, DurationToLongConverter            ...hotovo (AdminSecurityConfiguration - nutno upravit s ohledem na aktuální projekt)
+-složky: security, mapper  ...hotovo
+
+    AKTUÁLNĚ ŘEŠÍM: ExerciseEntity a TrainingEntity (posléze mě čeká DTO)
+ */
--- src/test/java/com/ragnarok/ragnarok_customers_training_diary/RagnarokCustomersTrainingDiaryApplicationTests.java
+++ src/test/java/com/ragnarok/ragnarok_customers_training_diary/RagnarokCustomersTrainingDiaryApplicationTests.java
@@ -1,4 +1,4 @@
-package com.ragnarok.ragnarokcustomerstrainingdiary;
+package com.ragnarok.ragnarok_customers_training_diary;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
